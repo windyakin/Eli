715ab25b987c5f9b7eea7c3de7a2ff8f83706631
windyakin <windyakin@gmail.com>
2016-05-05 20:21:07 +0900

    change output dir path format

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 
-var DEST_DIR = 'dev/'
+var DEST_DIR = 'dev';
 
 gulp.task('default', function() {
 	console.log("Hello, world!");
@@ -16,16 +16,16 @@ gulp.task('bower-install', function() {
 
 // bower libs copy `lib/` directory
 gulp.task('bower-dest', ['bower-install'], function() {
-	return gulp.src('./bower.json')
+	return gulp.src(['./bower.json'])
 		.pipe(plugins.mainBowerFiles({
 			includeDev: true
 		}))
 		.pipe(plugins.regexRename(/\/dist\//, '/'))
-		.pipe(gulp.dest('./'+ DEST_DIR +'lib'));
+		.pipe(gulp.dest('./'+ DEST_DIR +'/lib'));
 });
 
 gulp.task('release', function() {
-	DEST_DIR = 'dist/'
+	DEST_DIR = 'dist'
 });
 
 // mixed tasks
