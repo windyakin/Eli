02f3612e92ef92ca6b95216764a0005c444ef040
windyakin <windyakin@gmail.com>
2016-05-05 14:39:47 +0900

    add gulp
    gulp で同等の機能を開発できればなとおもってやってる

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
+
+var DEST_DIR = 'dev/'
+
+gulp.task('default', function() {
+	console.log("Hello, world!");
+});
+
+gulp.task('bower', function() {
+	return gulp.src('./bower.json').pipe(plugins.mainBowerFiles({
+		includeDev: true
+	}))
+	.pipe(plugins.regexRename(/dist\//, ''))
+	.pipe(gulp.dest('./'+ DEST_DIR +'lib'));
+});
+
+gulp.task('release', function() {
+	DEST_DIR = 'dist/'
+});
