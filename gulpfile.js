8188c7571a4e979a9f7be6c7d0be3d608bc7bc33
windyakin <windyakin@gmail.com>
2016-05-05 15:06:26 +0900

    add comment and putting in order

--- gulpfile.js
+++ gulpfile.js
@@ -9,10 +9,12 @@ gulp.task('default', function() {
 	console.log("Hello, world!");
 });
 
+// bower install
 gulp.task('bower-install', function() {
 	return plugins.bower();
 });
 
+// bower libs copy `lib/` directory
 gulp.task('bower-dest', ['bower-install'], function() {
 	return gulp.src('./bower.json')
 		.pipe(plugins.mainBowerFiles({
@@ -22,8 +24,9 @@ gulp.task('bower-dest', ['bower-install'], function() {
 		.pipe(gulp.dest('./'+ DEST_DIR +'lib'));
 });
 
-gulp.task('bower', ['bower-install', 'bower-dest']);
-
 gulp.task('release', function() {
 	DEST_DIR = 'dist/'
 });
+
+// mixed tasks
+gulp.task('bower', ['bower-install', 'bower-dest']);
