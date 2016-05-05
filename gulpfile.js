90ea6fb3265368a39c42ed76066d5b12b5b986d2
windyakin <windyakin@gmail.com>
2016-05-05 14:54:42 +0900

    bower install を前もって行うように

--- gulpfile.js
+++ gulpfile.js
@@ -9,13 +9,20 @@ gulp.task('default', function() {
 	console.log("Hello, world!");
 });
 
-gulp.task('bower', function() {
-	return gulp.src('./bower.json').pipe(plugins.mainBowerFiles({
-		includeDev: true
-	}))
-	.pipe(plugins.regexRename(/dist\//, ''))
-	.pipe(gulp.dest('./'+ DEST_DIR +'lib'));
+gulp.task('bower-install', function() {
+	return plugins.bower();
 });
+
+gulp.task('bower-dest', ['bower-install'], function() {
+	return gulp.src('./bower.json')
+		.pipe(plugins.mainBowerFiles({
+			includeDev: true
+		}))
+		.pipe(plugins.regexRename(/dist\//, ''))
+		.pipe(gulp.dest('./'+ DEST_DIR +'lib'));
+});
+
+gulp.task('bower', ['bower-install', 'bower-dest']);
 
 gulp.task('release', function() {
 	DEST_DIR = 'dist/'
